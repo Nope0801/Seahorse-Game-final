441ea2e6bfb823c78d430350137e05f7131cfbd0
Duckcyzzz <ducbanca1604@gmail.com>
2025-04-07 21:15:23 +0700

    add: demo Menu

--- src/com/seahorse/Main.java
+++
@@ -1,9 +0,0 @@
-package com.seahorse;
-
-import com.seahorse.view.GameFrame;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        GameFrame gameFrame = new GameFrame();
-    }
-}
---
+++ src/com/seahorse/view/guide_assets/step_1.java
@@ -0,0 +1,5 @@
+package com.seahorse.view.guide_assets;
+
+public class step_1 {
+
+}
